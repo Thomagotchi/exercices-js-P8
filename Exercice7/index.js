4cb3188cd96a273dd8520ea25498c2901e0b3148
Thomagotchi <thomassano87@gmail.com>
2025-09-12 12:05:17 +0200

    Update - Global -  Added comments

--- Exercice7/index.js
+++ Exercice7/index.js
@@ -1,11 +1,17 @@
 function convertToBinary() {
+  // Récupère la valeur
   const decimalInput = document.getElementById("decimalInput").value;
+
+  // Récupère l'élément HTML où sera affiché le résultat binaire
   const binaryResult = document.getElementById("binaryResult");
 
+  // Si le champ est vide ou la valeur n'est pas un nombre valide efface le contenu de l'élément de résultat
   if (decimalInput === "" || isNaN(decimalInput)) {
     binaryResult.textContent = "";
     return;
   }
 
+  // Conversion du nombre décimal en chaîne binaire
+  // https://stackoverflow.com/questions/9939760/how-do-i-convert-an-integer-to-binary-in-javascript
   binaryResult.innerText = (decimalInput >>> 0).toString(2);
 }
